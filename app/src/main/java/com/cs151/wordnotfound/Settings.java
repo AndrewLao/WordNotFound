dc8a5263ac3f7f58e7048fa985b040fc51def24c
AndrewLao <andrewlao765@gmail.com>
2021-04-20 20:41:06 -0700

    dark theme

--- app/src/main/java/com/cs151/wordnotfound/Settings.java
+++ app/src/main/java/com/cs151/wordnotfound/Settings.java
@@ -1,14 +1,18 @@
 package com.cs151.wordnotfound;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,34 @@ public class Settings extends AppCompatActivity {
                 startActivity(new Intent(Settings.this, MainActivity.class));
             }
         });
+
+        Switch darkSwitch = (Switch) findViewById(R.id.setting_dark);
+        sharedPreferences = getSharedPreferences("night", 0);
+        Boolean isNight = sharedPreferences.getBoolean("night_mode", true);
+
+        if(isNight){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            darkSwitch.setChecked(true);
+        }
+
+        darkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    darkSwitch.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode", true);
+                    editor.commit();
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    darkSwitch.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode", false);
+                    editor.commit();
+                }
+            }
+        });
+
     }
 }
