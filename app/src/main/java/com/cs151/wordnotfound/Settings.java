9e9f3bf5f1cfe9975cab5b59784b78465c032a6e
AndrewLao <andrewlao765@gmail.com>
2021-05-06 22:44:34 -0700

    working dark-theme

--- app/src/main/java/com/cs151/wordnotfound/Settings.java
+++ app/src/main/java/com/cs151/wordnotfound/Settings.java
@@ -10,46 +10,73 @@ import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
 
-public class Settings extends AppCompatActivity {
-    SharedPreferences sharedPreferences = null;
+public class Settings extends AppCompatActivity implements View.OnClickListener{
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    private static String DARK_MODE = "Dark_mode_prefs";
+    private static String NIGHT = "night_on";
+    private static String SWITCH_STATUS = "switch status";
+
+    boolean switchStatus;
+    boolean darkStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        sharedPreferences = getSharedPreferences(DARK_MODE, MODE_PRIVATE);
+        editor = getSharedPreferences(DARK_MODE, MODE_PRIVATE).edit();
+
         ImageButton homeButton = (ImageButton) findViewById(R.id.settings_main);
-        homeButton.setOnClickListener(new View.OnClickListener() {
+        homeButton.setOnClickListener(this);
+
+        Switch darkSwitch = (Switch) findViewById(R.id.setting_dark);
+
+        switchStatus = sharedPreferences.getBoolean(SWITCH_STATUS, false); // light mode default
+        darkStatus = sharedPreferences.getBoolean(NIGHT, false);
+
+        darkSwitch.setChecked(switchStatus);
+
+        if(darkStatus){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        darkSwitch.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v) {
-                startActivity(new Intent(Settings.this, MainActivity.class));
+                if(darkSwitch.isChecked()){
+                    editor.putBoolean(SWITCH_STATUS, true);
+                    editor.putBoolean(NIGHT, true);
+                    darkSwitch.setChecked(true);
+                    editor.apply();
+                    recreate();
+                }else{
+                    editor.putBoolean(SWITCH_STATUS, false);
+                    editor.putBoolean(NIGHT, false);
+                    darkSwitch.setChecked(false);
+                    editor.apply();
+                    recreate();
+                }
             }
         });
+    }
 
-//        Switch darkSwitch = (Switch) findViewById(R.id.setting_dark);
-//        sharedPreferences = getSharedPreferences("night", 0);
-//        Boolean isNight = sharedPreferences.getBoolean("night_mode", false);
-//
-//        if(isNight){
-//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//            darkSwitch.setChecked(true);
-//        }
-//
-//        darkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if(isChecked){
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//                    darkSwitch.setChecked(true);
-//                    SharedPreferences.Editor editor = sharedPreferences.edit();
-//                    editor.putBoolean("night_mode", true);
-//                    editor.commit();
-//                }else{
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-//                    darkSwitch.setChecked(false);
-//                    SharedPreferences.Editor editor = sharedPreferences.edit();
-//                    editor.putBoolean("night_mode", false);
-//                    editor.commit();
-//                }
-//            }
-//        });
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case (R.id.settings_main):
+                openMain();
+                break;
+        }
+    }
+
+
+
+
+    public void openMain(){
+        startActivity(new Intent(Settings.this, MainActivity.class));
     }
 }
