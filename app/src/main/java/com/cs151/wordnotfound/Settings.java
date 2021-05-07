a6a0900003dcee615b073aa9edce790a91c5c67d
AndrewLao <andrewlao765@gmail.com>
2021-05-06 20:53:11 -0700

    cleaned up main

--- app/src/main/java/com/cs151/wordnotfound/Settings.java
+++ app/src/main/java/com/cs151/wordnotfound/Settings.java
@@ -2,7 +2,6 @@ package com.cs151.wordnotfound;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -25,33 +24,32 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-        Switch darkSwitch = (Switch) findViewById(R.id.setting_dark);
-        sharedPreferences = getSharedPreferences("night", 0);
-        Boolean isNight = sharedPreferences.getBoolean("night_mode", true);
-
-        if(isNight){
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            darkSwitch.setChecked(true);
-        }
-
-        darkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    darkSwitch.setChecked(true);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("night_mode", true);
-                    editor.commit();
-                }else{
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    darkSwitch.setChecked(false);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("night_mode", false);
-                    editor.commit();
-                }
-            }
-        });
-
+//        Switch darkSwitch = (Switch) findViewById(R.id.setting_dark);
+//        sharedPreferences = getSharedPreferences("night", 0);
+//        Boolean isNight = sharedPreferences.getBoolean("night_mode", false);
+//
+//        if(isNight){
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//            darkSwitch.setChecked(true);
+//        }
+//
+//        darkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked){
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                    darkSwitch.setChecked(true);
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putBoolean("night_mode", true);
+//                    editor.commit();
+//                }else{
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    darkSwitch.setChecked(false);
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putBoolean("night_mode", false);
+//                    editor.commit();
+//                }
+//            }
+//        });
     }
 }
