3afd49bffc87d493184f7764d737d473b8d768e7
AndrewLao <andrewlao765@gmail.com>
2021-05-06 23:05:14 -0700

    global dark

--- app/src/main/java/com/cs151/wordnotfound/MainActivity.java
+++ app/src/main/java/com/cs151/wordnotfound/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -14,11 +15,24 @@ import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    SharedPreferences sharedPreferences;
+    boolean darkStatus;
+    private static String DARK_MODE = "Dark_mode_prefs";
+    private static String NIGHT = "night_on";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences(DARK_MODE, MODE_PRIVATE);
+        darkStatus = sharedPreferences.getBoolean(NIGHT, false);
+
+        if(darkStatus){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
 
         Button regButton = (Button) findViewById(R.id.mainRegularLevel);
         regButton.setOnClickListener(this);
