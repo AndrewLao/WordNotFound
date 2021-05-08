3391f347c7a33d7f551b2510f7c5b252cda86cc7
brluu412 <brluu412@gmail.com>
2021-05-08 16:11:32 -0700

    implemented WordBank

--- app/src/main/java/com/cs151/wordnotfound/Word.java
+++ app/src/main/java/com/cs151/wordnotfound/Word.java
@@ -2,17 +2,20 @@ package com.cs151.wordnotfound;
 
 public class Word {
     private String word;
-    private Score wordScore;
+    private Score score;
 
     public Word (String word) {
         this.word = word;
+        score = new Score();
+        score.setScore(word.length() * 100);
+
     }
 
     public String getWord() {
         return word;
     }
 
-    public Score getScore() {
-        return wordScore;
+    public Score getWordScore() {
+        return score;
     }
 }
--- app/src/main/java/com/cs151/wordnotfound/WordBank.java
+++ app/src/main/java/com/cs151/wordnotfound/WordBank.java
@@ -1,22 +1,129 @@
 package com.cs151.wordnotfound;
-import java.util.ArrayList;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class WordBank {
     private ArrayList<Word> bank;
+    private int level;
+    private String format;
 
-    public WordBank(String level) {
-        readFile(level);
+    private String levelFile;
+    private boolean levelSelect;//FALSE FOR REG, TRUE FOR LEVEL
+    private File f;
+    private FileReader fr;
+    private BufferedReader br;
+    private ArrayList<WordBank> wordBankList;
+
+    public WordBank(int level, ArrayList<Word> bank, String format) {
+        this.level = level;
+        this.bank = bank;
+        this.format = format;
     }
 
-    //FileReader will be used to read a file containing the words being used for each level.
-    //New Word object constructed for each word read from the file will be placed into ArrayList bank.
-    //Parameter String level is the name of the level notepad
-    public void readFile(String level) {
 
+    public WordBank(boolean levelSelect){
+        this.levelSelect = levelSelect;
+        wordBankList = new ArrayList<>();
+
+        if(levelSelect) {
+            levelFile = "src/gravLevelSelect.txt";
+        }else{
+            levelFile = "src/regLevelSelect.txt";
+        }
+        try {
+
+            f = new File(levelFile);
+            fr = new FileReader(f);
+            br = new BufferedReader(fr);
+
+            boolean endOfFile = true;
+
+            while (endOfFile) {
+                /*
+                line 1 = level
+                line 2 = word1, word2, word3
+                line 3 = format of grid
+                 */
+
+                String str = br.readLine();				//level in line 1
+
+
+                if(str == null) {						//checks if line read is null, break;
+                    endOfFile = false;
+                    break;
+                }
+
+                level = Integer.parseInt(str);          //reads level of word in line 1
+
+                bank = new ArrayList<Word>();               //new arraylist
+                str = br.readLine();					//line 2
+                String[] comma = str.split(", ");       //splits line 2
+
+                for(String k: comma) {                  //fills up the bank in our Wordbank object
+                    Word word = new Word(k);
+                    bank.add(word);
+
+                }
+                str = br.readLine();
+                format = str;					//line 3
+
+                System.out.println(toString());
+
+                wordBankList.add(new WordBank(level, bank, format));
+            }
+
+            //close readers
+            br.close();
+            fr.close();
+
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    public ArrayList<Word> getbank() {
-        return (ArrayList)bank.clone();
+    /**
+     * method returns WordBank Object based on level chosen
+     * @param pLevel
+     * @return Wordbank obj
+     */
+    public WordBank getLevel(int pLevel) {
+        return wordBankList.get(pLevel + 1);
+    }
+
+    /**
+     * gets format of characters for gridview
+     * @return
+     */
+    public String getFormat(){
+        return format;
+    }
+
+    /**
+     * returns list of words in the wordbank
+     * @return
+     */
+    public ArrayList<Word> getBank(){
+        return bank;
+    }
+
+    /**
+     * returns all levels/wordBanks
+     * @return
+     */
+    public ArrayList<WordBank> getWordBank(){
+        return wordBankList;
+    }
+
+    /**
+     * toString
+     * @return
+     */
+    public String toString() {
+        String str = "level = " + level + "\nwordBank = ";
+        for(Word k: bank) {
+            str += k.getWord() + ", ";
+        }
+        return str += "\nformat = " + format;
     }
 }
